a640f0482cacc9fa4781061f268473ce34300d94
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-02-27 10:49:57 -0300

    Solid

---
+++ src/br/com/jackson/rh/service/reajuste/PromocaoService.java
@@ -0,0 +1,18 @@
+package br.com.jackson.rh.service.reajuste;
+
+import br.com.jackson.rh.ValidacaoException;
+import br.com.jackson.rh.model.Cargo;
+import br.com.jackson.rh.model.Funcionario;
+
+public class PromocaoService {
+
+    public void promover(Funcionario funcionario, boolean metaBatida){
+        Cargo cargoAtual = funcionario.getCargo();
+        if (Cargo.GERENTE == cargoAtual){
+            throw new ValidacaoException("Gerente não podem ser promovidos");
+        }
+        if (metaBatida){
+            Cargo novoCargo = cargoAtual.getProximoCargo();
+        }
+    }
+}
